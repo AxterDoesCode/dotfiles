47d6d4be262340d68403988f75874f938c9840dd
AxterDoesCode <chau.alex88@hotmail.com>
2023-06-21 21:13:22 +0100

    -added nvim config

---
+++ dot_config/nvim/lua/custom/plugins.lua
@@ -0,0 +1,45 @@
+local cmp = require "cmp"
+
+local plugins = {
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "gopls",
+        "golines",
+        "goimports-reviser",
+        "gofumpt"
+      },
+    },
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "custom.configs.lspconfig"
+    end,
+  },
+  {
+    "jose-elias-alvarez/null-ls.nvim",
+    ft = "go",
+    opts = function()
+      return require "custom.configs.null-ls"
+    end,
+  },
+  {
+    "mfussenegger/nvim-dap",
+  },
+  {
+    "leoluz/nvim-dap-go",
+    ft = "go",
+    dependencies = "mfussenegger/nvim-dap",
+    config = function(_, opts)
+      require("dap-go").setup(opts)
+    end
+  },
+}
+return plugins
