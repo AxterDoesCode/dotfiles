ee3efc8229e61b878642051514ece5b63a67b227
AxterDoesCode <chau.alex88@hotmail.com>
2023-07-04 13:46:12 +0100

    Space paste bind added

--- dot_config/nvim/lua/custom/mappings.lua
+++ dot_config/nvim/lua/custom/mappings.lua
@@ -6,10 +6,14 @@ M.general = {
     ["<C-l>"] = { "<cmd> TmuxNavigateRight<CR>", "window right"},
     ["<C-j>"] = { "<cmd> TmuxNavigateDown<CR>", "window down"},
     ["<C-k>"] = { "<cmd> TmuxNavigateUp<CR>", "window up"},
-    ["<C-d>"] = {"<C-d>zz"},
-    ["<C-u>"] = {"<C-u>zz"},
+    ["<C-d>"] = { "<C-d>zz"},
+    ["<C-u>"] = { "<C-u>zz"},
     ["n"] = {"nzzzv"},
     ["N"] = {"Nzzzv"}
+  },
+
+  x = {
+    ["<leader>p"] = {"\"_dP", "Paste without removing buffer"}
   }
 }
 
