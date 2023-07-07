62b568fec06f01df9063e107ee59f20725ad74b8
AxterDoesCode <chau.alex88@hotmail.com>
2023-07-07 20:17:57 +0100

    jesus

--- dot_config/nvim/lua/custom/plugins.lua
+++ dot_config/nvim/lua/custom/plugins.lua
@@ -120,10 +120,10 @@ local plugins = {
         -- to make this work, you need to set the following kitty options:
         -- - allow_remote_control socket-only
         -- - listen_on unix:/tmp/kitty
-        kitty = {
-          enabled = true,
-          font = "+4", -- font size increment
-        },
+      alacritty = {
+        enabled = true,
+        font = "14", -- font size
+    },
       }
     }
   },
