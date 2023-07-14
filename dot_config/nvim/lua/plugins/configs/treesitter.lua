98343da305c26a8df4c031cc483491a293ba3178
AxterDoesCode <chau.alex88@hotmail.com>
2023-07-14 17:08:40 +0100

    Replaced default neovim config

--- dot_config/nvim/lua/plugins/configs/treesitter.lua
+++ dot_config/nvim/lua/plugins/configs/treesitter.lua
@@ -1,10 +1,13 @@
 local options = {
-  ensure_installed = { "lua" },
-
-  highlight = {
-    enable = true,
-    use_languagetree = true,
-  },
+    ensure_installed = {"go","javascript", "typescript", "c", "lua", "rust" },
+    highlight = {
+        enable = true,
+        use_languagetree = true,
+        additional_vim_regex_highlighting = false,
+    },
+    sync_install = false,
+    auto_install = true,
+    
 
   indent = { enable = true },
 }
