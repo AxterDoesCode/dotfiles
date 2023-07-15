774610ed7565d33db054ff2ad243bd74576b441b
AxterDoesCode <chau.alex88@hotmail.com>
2023-07-15 19:57:55 +0100

    Please work

--- dot_config/nvim/lua/plugins/init.lua
+++ dot_config/nvim/lua/plugins/init.lua
@@ -1,4 +1,4 @@
-return{
+return {
     {
         "nvim-treesitter/nvim-treesitter",
         lazy = false,
@@ -21,8 +21,8 @@ return{
         branch = 'v2.x',
         dependencies = {
             -- LSP Support
-            {'neovim/nvim-lspconfig'},             -- Required
-            {                                      -- Optional
+            { 'neovim/nvim-lspconfig' }, -- Required
+            {                          -- Optional
                 'williamboman/mason.nvim',
                 cmd = { "Mason", "MasonInstall", "MasonInstallAll", "MasonUninstall", "MasonUninstallAll", "MasonLog" },
                 build = ":MasonUpdate",
@@ -30,39 +30,46 @@ return{
                     return require "plugins.configs.mason"
                 end,
             },
-            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
             -- Autocompletion
-            {'hrsh7th/nvim-cmp'},     -- Required
-            {'hrsh7th/cmp-nvim-lsp'}, -- Required
-            {'L3MON4D3/LuaSnip'},     -- Required
+            { 'hrsh7th/nvim-cmp' },   -- Required
+            { 'hrsh7th/cmp-nvim-lsp' }, -- Required
+            { 'L3MON4D3/LuaSnip' },   -- Required
         },
         config = function()
             require("plugins.configs.lsp-zero")
         end,
     },
     {
-        'nvim-telescope/telescope.nvim', tag = '0.1.2',
+        'nvim-telescope/telescope.nvim',
+        tag = '0.1.2',
         -- or                              , branch = '0.1.x',
         dependencies = { 'nvim-lua/plenary.nvim' },
         keys = {
-            { "<leader>pf", "<cmd>Telescope find_files<cr>", desc = "Find files" },
-            { "<C-p>", "<cmd>Telescope git_files<cr>", desc = "Git files" },
-            { "<leader>ps",function() require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") }) end, desc = "Grep string" },
-            { "<leader>vh", "<cmd>Telescope help_tags<cr>", desc = "Vim help tags" },
+            { "<leader>pf", "<cmd>Telescope find_files<cr>",                                                               desc =
+            "Find files" },
+            { "<C-p>",      "<cmd>Telescope git_files<cr>",                                                                desc =
+            "Git files" },
+            { "<leader>ps",
+                                function() require("telescope.builtin").grep_string({ search = vim.fn.input("Grep > ") }) end,
+                                                                                                                               desc =
+                "Grep string" },
+            { "<leader>vh", "<cmd>Telescope help_tags<cr>",                                                                desc =
+            "Vim help tags" },
         }
     },
     {
         'nvim-lualine/lualine.nvim',
         lazy = false,
-        dependencies = { 'nvim-tree/nvim-web-devicons'},
+        dependencies = { 'nvim-tree/nvim-web-devicons' },
         config = function()
             require("lualine").setup()
         end,
     },
     {
         "folke/trouble.nvim",
-        cmd = {"Trouble", "TroubleToggle"},
+        cmd = { "Trouble", "TroubleToggle" },
         config = function()
             require("trouble").setup {
                 icons = false,
@@ -72,24 +79,24 @@ return{
             }
         end,
         keys = {
-            {"<leader>xq", "<cmd>TroubleToggle quickfix<cr>", silent = true, noremap = true},
+            { "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", silent = true, noremap = true },
         }
     },
     {
         "theprimeagen/harpoon",
         lazy = false,
         keys = {
-            { "<leader>a",function() require("harpoon.mark").add_file() end, desc = "Add file to harpoon" },
-            { "<C-e>",function() require("harpoon.ui").toggle_quick_menu() end, desc = "Toggle harpoon menu" },
-            { "<C-h>",function() require("harpoon.ui").nav_file(1) end, desc = "Go to harpoon file 1" },
-            { "<C-t>",function() require("harpoon.ui").nav_file(2) end, desc = "Go to harpoon file 2" },
-            { "<C-n>",function() require("harpoon.ui").nav_file(3) end, desc = "Go to harpoon file 3" },
-            { "<C-s>",function() require("harpoon.ui").nav_file(4) end, desc = "Go to harpoon file 4" },
+            { "<leader>a", function() require("harpoon.mark").add_file() end,        desc = "Add file to harpoon" },
+            { "<C-e>",     function() require("harpoon.ui").toggle_quick_menu() end, desc = "Toggle harpoon menu" },
+            { "<C-h>",     function() require("harpoon.ui").nav_file(1) end,         desc = "Go to harpoon file 1" },
+            { "<C-t>",     function() require("harpoon.ui").nav_file(2) end,         desc = "Go to harpoon file 2" },
+            { "<C-n>",     function() require("harpoon.ui").nav_file(3) end,         desc = "Go to harpoon file 3" },
+            { "<C-s>",     function() require("harpoon.ui").nav_file(4) end,         desc = "Go to harpoon file 4" },
         },
     },
     {
         "mbbill/undotree",
-        cmd = {"Undotree", "UndotreeToggle"},
+        cmd = { "Undotree", "UndotreeToggle" },
         keys = {
             { "<leader>u", "<cmd>UndotreeToggle<cr>", desc = "Toggle Undotree" },
         },
@@ -98,34 +105,44 @@ return{
         "folke/zen-mode.nvim",
         lazy = false,
         keys = {
-            { "<leader>zz",function() require("zen-mode").toggle({
-                window = {
-                    width = 90,
-                    options = { }
-                },
-            })
-                vim.wo.wrap = false
-                vim.wo.number = true
-                vim.wo.rnu = true
-            end, desc = "Toggle Zen-mode" },
-            { "<leader>zZ",function() require("zen-mode").toggle({
-                window = {
-                    width = 80,
-                    options = { }
-                },
-            })
-                vim.wo.wrap = false
-                vim.wo.number = false
-                vim.wo.rnu = false
-                vim.opt.colorcolumn = "0"
-            end, desc = "Toggle Zen-mode" },
+            {
+                "<leader>zz",
+                function()
+                    require("zen-mode").toggle({
+                        window = {
+                            width = 90,
+                            options = {}
+                        },
+                    })
+                    vim.wo.wrap = false
+                    vim.wo.number = true
+                    vim.wo.rnu = true
+                end,
+                desc = "Toggle Zen-mode"
+            },
+            {
+                "<leader>zZ",
+                function()
+                    require("zen-mode").toggle({
+                        window = {
+                            width = 80,
+                            options = {}
+                        },
+                    })
+                    vim.wo.wrap = false
+                    vim.wo.number = false
+                    vim.wo.rnu = false
+                    vim.opt.colorcolumn = "0"
+                end,
+                desc = "Toggle Zen-mode"
+            },
         }
     },
     {
         "tpope/vim-fugitive",
         lazy = false,
         keys = {
-            {"<leader>gs", "<cmd>Git<cr>", desc = "Check Git status"},
+            { "<leader>gs", "<cmd>Git<cr>", desc = "Check Git status" },
         }
     },
     {
@@ -156,12 +173,20 @@ return{
     },
     {
         "lambdalisue/suda.vim",
-        cmd = {"SudaWrite", "SudaRead"},
+        cmd = { "SudaWrite", "SudaRead" },
     },
     {
         'eandrju/cellular-automaton.nvim',
-        cmd = {"CellularAutomaton"},
+        cmd = { "CellularAutomaton" },
+    },
+    {
+        "olexsmir/gopher.nvim",
+        ft = "go",
+        config = function(_, opts)
+            require("gopher").setup(opts)
+        end,
+        build = function()
+            vim.cmd [[silent! GoInstallDeps]]
+        end,
     },
 }
-
-
