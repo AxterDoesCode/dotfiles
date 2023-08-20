f9ceb5d3f24b1df654028ff2fb3431c4d5149629
axtr <chaualex88@hotmail.com>
2023-08-20 17:08:37 +0100

    Keybinds changed

--- dot_config/nvim/lua/axtr/remaps.lua
+++ dot_config/nvim/lua/axtr/remaps.lua
@@ -31,7 +31,7 @@ vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
 vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
 
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], {desc = "Replace every word"})
-vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true, desc = "Make executable"} )
+vim.keymap.set("n", "<leader>xx", "<cmd>!chmod +x %<CR>", { silent = true, desc = "Make executable"} )
 
 vim.keymap.set("n", "<leader>mr", "<cmd>CellularAutomaton make_it_rain<CR>", { desc = "Make it rain" });
 
